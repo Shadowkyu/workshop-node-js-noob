d1028529d14a2c6f734a75777a5bf7374b38251b
quentin Dijoux <quentin.dijoux@ynov.com>
2017-11-21 16:52:05 +0100

    Exercice 4

--- src/exercise4.js
+++ src/exercise4.js
@@ -1,9 +1,8 @@
 exports.run = function(input) {
-  console.log("Exercise #4");
-
   // YOUR CODE IS WELCOME HERE
-
+  function uno (a,b,c){
+  return "Beetlejuice".repeat(3);
   /** STRICTLY FORBIDDEN TO CHANGE THIS CODE */
   return input(3, "Beetlejuice", repeat);
-  /** */
+  /** */}
 };
