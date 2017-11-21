2620386b1dbfedaedbc79b3bd14b764ffeade86d
quentin Dijoux <quentin.dijoux@ynov.com>
2017-11-21 16:39:06 +0100

    Exercice 3

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,3 +1,28 @@
 exports.run = function(input) {
-  console.log("Exercise #3");
+	var multiply = new Boolean("false");
+	function calcul (a, b, c){
+	  	var a = 2;
+	  	var b = 5;
+	  	var c = 3;
+	  	var result = 6;
+	  	if (a*b <= 6){
+	  		multiply = true;
+	  	}
+	  	else{
+	  		multiply = false;
+	  	}
+	  	if (a*c <= 6){
+	  		multiply = true;
+	  	}
+	  	else{
+	  		multiply = false;
+	  	}
+	  	if (b*c <= 6){
+	  		multiply = true;
+	  	}
+	  	else{
+	  		multiply = false;
+	  	}
+	return (result);
+    }
 };
