a2e4530236c43f3f077afd06b6afcd21ccca8471
quentin Dijoux <quentin.dijoux@ynov.com>
2017-11-21 17:03:08 +0100

    Exercice 5

--- src/exercise5.js
+++ src/exercise5.js
@@ -1,3 +1,13 @@
+var fs = require('fs');
+var path = require('path');
+
 exports.run = function(input) {
-  console.log("Exercise #5");
+
+	var buf = fs.readdir(process.argv[2], function callback(err, files){
+		for (var i = 0; i < files.length; i++) {
+		if(path.extname(files[i]) === '.'+process.argv[3]){
+			console.log(files[i])
+		}
+	}	
+});
 };
